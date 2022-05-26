d6304a691005059063d86b5a5eb27d62dc011358
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-26 15:40:04 +0530

    1 file modified, 1 added

--- myapp/routes/courses.routes.js
+++ myapp/routes/courses.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-var courses = require('../model/course.model');
+const courses = require('../model/course.model');
 
-let router = express.Router();
+const router = express.Router();
 
 router.route('/').get((req, res) => {
   res.render('courses', { listofcourses: courses, title: 'List Of Courses' });
+});
+
+router.route('/coursedetails/:id').get((req, res) => {
+  const courseId = +req.params.id;
+  const course = courses.find((course) => course.id === courseId);
+  res.render('coursedetails', { course });
 });
 
 module.exports = router;
